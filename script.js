d3a162bcfba16000dce92a40b0cc2107d4f2bd07
LapisRazuri <120453194+LapisRazuri@users.noreply.github.com>
2023-10-12 10:43:10 +0200

    limits 8 items to appear on the display else will render error

--- script.js
+++ script.js
@@ -1,3 +1,20 @@
+let btn1 = document.getElementsByClassName("btn1");
+let btn2 = document.getElementsByClassName("btn2");
+let btn3 = document.getElementsByClassName("btn3");
+let btn4 = document.getElementsByClassName("btn4");
+let btn5 = document.getElementsByClassName("btn5");
+let btn6 = document.getElementsByClassName("btn6");
+let btn7 = document.getElementsByClassName("btn7");
+let btn8 = document.getElementsByClassName("btn8");
+let btn9 = document.getElementsByClassName("btn9");
+let btn0 = document.getElementsByClassName("btn0");
+let btnMinus = document.getElementsByClassName("btnMinus");
+let btnPlus = document.getElementsByClassName("btnPlus");
+let btnMultiply = document.getElementsByClassName("btnMultiply");
+let btnDivide = document.getElementsByClassName("btnDivide");
+let display = document.getElementsByClassName("display");
+let btns = document.getElementsByTagName("button")
+
 const add = (a, b) => a + b;
 console.log(add(3, 7))
 
@@ -28,6 +45,7 @@ function operate (a, op, b) {
         add(a, b)
     }
 
+
     else if ( op == "-" ) {
         subtract(a, op, b)
     }
@@ -40,6 +58,43 @@ function operate (a, op, b) {
         divide(a, op, b)
     }
 }
+
+
+
+
+
+function displayValue (textContOfButton) {
+   
+    let maxitems = 8;
+    let equation = [];      // console.log(Array.isArray(equation)) works but typeof equation gives 'undefined'
+    let 
+    
+    
+    if (display[0].textContent.length < maxitems)  {
+        equation.push(textContOfButton)
+        display[0].textContent += equation;
+        console.log(Array.isArray(equation))
+    }
+
+    else { 
+        display[0].textContent = "ERROR";
+        display[0].style.color = "red"
+    }
+       
+       
+        }   
+    
+        
+   
+     
+
+
+let buttonArray = Array.from(btns);
+buttonArray.forEach( function (button) {
+    button.addEventListener("click", function () {
+        displayValue(button.textContent)
+    })
+})
 
 
 
